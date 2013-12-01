c74df7423bf35c7933dab57a1c3889122a878205
dead_horse <dead_horse@qq.com>
2013-12-01 16:33:16 +0800

    Initial Commit

---
+++ shentie/shentie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shentie
+//
+//  Created by he yiyu on 13-12-1.
+//  Copyright (c) 2013年 wanshanglaiwojiachifan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shentie/shentieTests/shentieTests.m
@@ -0,0 +1,34 @@
+//
+//  shentieTests.m
+//  shentieTests
+//
+//  Created by he yiyu on 13-12-1.
+//  Copyright (c) 2013年 wanshanglaiwojiachifan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface shentieTests : XCTestCase
+
+@end
+
+@implementation shentieTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
